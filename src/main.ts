d9bd6322c7c65d7e5e08e27e6e6bb652d09ba4bf
Занегин Никита <zaneginis@gmail.com>
2026-02-23 12:24:41 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withHashLocation())
+  ]
 }).catch(err => console.error(err));
